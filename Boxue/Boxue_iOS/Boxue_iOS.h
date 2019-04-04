5310ad6345a1daf95db9237e01f1a0f7d4e650c8
tyih <tyih@icloud.com>
2019-04-04 10:12:34 +0800

    框架搭建

---
+++ Boxue/Boxue_iOS/Boxue_iOS.h
@@ -0,0 +1,19 @@
+//
+//  Boxue_iOS.h
+//  Boxue_iOS
+//
+//  Created by tianyao on 2019/3/25.
+//  Copyright © 2019 tiya.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Boxue_iOS.
+FOUNDATION_EXPORT double Boxue_iOSVersionNumber;
+
+//! Project version string for Boxue_iOS.
+FOUNDATION_EXPORT const unsigned char Boxue_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Boxue_iOS/PublicHeader.h>
+
+
